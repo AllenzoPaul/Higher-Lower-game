7334d0aa78604d816cdff88c7e36610aace1f46d
Allenzo Paul J R <allenzopaul.jr@gmail.com>
2024-10-25 20:48:41 +0530

    added loop

--- main.py
+++ main.py
@@ -9,39 +9,45 @@ def format_data(account):
     account_country = account["country"]
     return f"{account_name}, a {account_descr} from {account_country}"
 
-def check_answer(guess, a_followers, b_followers):
+def check_answer(user_guess, a_followers, b_followers):
     if a_followers > b_followers:
-        return guess == a
+        return user_guess == "a"
     else:
-        return guess == b 
-
+        return user_guess == "b"
+        
 
 #display the logo 
 print(logo)
 score = 0
+continue_game = True
 
-#generate a random account from the game data
-account_a = random.choice(data)
 account_b = random.choice(data)
-if account_a == account_b:
+
+while continue_game:
+    #generate a random account from the game data
+    account_a = account_b
     account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
 
-print(f"Compare A : {format_data(account_a)}.")
-print(vs)
-print(f"Against B : {format_data(account_b)}.")
+    print(f"Compare A : {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B : {format_data(account_b)}.")
 
-#ask user for a guess
-guess = input("Who has more followers? A or B").lower()
+    #ask user for a guess
+    guess = input("Who has more followers? A or B : ").lower()
 
-#get the followers of each account 
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    #get the followers of each account 
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
 
-#give user their feedback
-  
-if is_correct:
-    score += 1
-    print(f"You're right! Your current score: {score}")   
-else:
-    print(f"You're wrong! Your total score is: {score}")
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    #give user their feedback
+    
+    if is_correct:
+        score += 1
+        print(f"You're right! Your current score: {score}")   
+    else:
+        print(f"You're wrong! Your total score is: {score}")
+        continue_game = False
